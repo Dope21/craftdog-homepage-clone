6e9cccd0dbf627fd4597a2dd8399b8cf39ea2b20
Thanasak <bestzacap20@gmail.com>
2022-07-14 15:11:29 +0700

    initial commit

---
+++ pages/works/weather.js
@@ -0,0 +1,47 @@
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react/'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layout/article'
+
+const Work = () => {
+  return (
+    <Layout title="Weather">
+      <Container>
+        <Title>
+          Weather App <Badge>2022</Badge>
+        </Title>
+        <P>
+          A weather report app from OpenWeatherMap API. Support Server Side
+          Rendering, Searchbox filter, and smooth fade in animation.
+        </P>
+        <List ml={4} my={4}>
+          <ListItem>
+            <Meta>Stack</Meta>
+            <span>Next.js, Tailwind CSS, Framer motion</span>
+          </ListItem>
+          <ListItem>
+            <Meta>Live</Meta>
+            <Link href="https://weather-app-three-theta.vercel.app/">
+              https://weather-app-three-theta.vercel.app/{' '}
+              <ExternalLinkIcon mx="2px" />
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Meta>Source</Meta>
+            <Link href="https://github.com/Dope21/next-weather-app">
+              https://github.com/Dope21/next-weather-app{' '}
+              <ExternalLinkIcon mx="2px" />
+            </Link>
+          </ListItem>
+        </List>
+
+        <WorkImage src="/images/works/weather_01.png" alt="weather" />
+        <WorkImage src="/images/works/weather_02.png" alt="weather" />
+        <WorkImage src="/images/works/weather_03.png" alt="weather" />
+      </Container>
+    </Layout>
+  )
+}
+
+export default Work
