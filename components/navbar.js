728804cf580ac59c50148bcb0a42aefe4e4bf1ae
Thanasak <bestzacap20@gmail.com>
2022-07-14 11:29:23 +0700

    changed posts to skills

--- components/navbar.js
+++ components/navbar.js
@@ -72,8 +72,8 @@ const Navbar = props => {
           <LinkItem href="/works" path={path}>
             Works
           </LinkItem>
-          <LinkItem href="/posts" path={path}>
-            Posts
+          <LinkItem href="/skills" path={path}>
+            Skills
           </LinkItem>
         </Stack>
 
@@ -94,8 +94,8 @@ const Navbar = props => {
                 <NextLink href="/works" passHref>
                   <MenuItem as={Link}>Works</MenuItem>
                 </NextLink>
-                <NextLink href="/posts" passHref>
-                  <MenuItem as={Link}>Posts</MenuItem>
+                <NextLink href="/skills" passHref>
+                  <MenuItem as={Link}>Skills</MenuItem>
                 </NextLink>
                 <MenuItem
                   as={Link}
